2d5742db50d02dbb9c5cbd415e7931367f4f815a
nervevau2 <eugenshow83@gmail.com>
2022-05-05 19:19:13 +0300

    Finish

--- config/settings.py
+++ config/settings.py
@@ -179,7 +179,7 @@ EMAIL_PORT = env('EMAIL_PORT')
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": env('Re'),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
